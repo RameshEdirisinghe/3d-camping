97451b61ced10f26fe417e75445bd7a710921cdc
RameshEdirisinghe <e2145434@bit.uom.lk>
2025-02-11 21:35:30 +0530

    add text

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,14 @@
 import { OrbitControls } from "@react-three/drei";
+import { Text } from '@react-three/drei';
+
 
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
-      <Text font ={"fonts/Poppins-Black.ttf"} >
-        Camping{"\n"} Life
-        <meshBasicMaterial color="white" />
+      <Text font={"fonts/Poppins-Black.ttf"}>
+         MY CAMPING {"\n"}               LIFE
+        <meshBasicMaterial color= "white"/>
       </Text>
     </>
   );
