1e3d4bd4407c069124c9f0820c67124d1a8f21c4
RameshEdirisinghe <e2145434@bit.uom.lk>
2025-02-11 23:08:22 +0530

    add camp site

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,15 +1,25 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls } from "@react-three/drei";
 import { Text } from '@react-three/drei';
+import { Camping } from "./Camping";
 
 
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
-      <Text font={"fonts/Poppins-Black.ttf"}>
-         MY CAMPING {"\n"}               LIFE
+      <Text font={"fonts/Poppins-Black.ttf"}
+        position-x={-1.3}
+        position-y={-0.5}
+        position-z={1}
+        lineHeight={0.8}
+        textAlign="center"
+        // rotation-y={degToRad(30)}
+        anchorY={"bottom"}>
+         WHY  I  LOVE{"\n"}CAMPING
         <meshBasicMaterial color= "white"/>
       </Text>
+      <Camping />
+      <Environment preset="sunset" />
     </>
   );
 };
