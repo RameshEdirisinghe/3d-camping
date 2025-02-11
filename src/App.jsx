7ab819318d2be4e80315e0dbad80da595875d90b
RameshEdirisinghe <e2145434@bit.uom.lk>
2025-02-11 23:46:59 +0530

    add intro animation and glass effects

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import { Experience } from "./components/Experience";
 function App() {
   return (
     <Canvas shadows camera={{ position: [0, 0, 8], fov: 42 }}>
-      <color attach="background" args={["#112417"]} />
+      <color attach="background" args={["#171720"]} />
+      <fog attach="fog" args={["#171720", 10, 30]} />
       <Experience />
     </Canvas>
   );
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,24 +1,83 @@
-import { Environment, OrbitControls } from "@react-three/drei";
-import { Text } from '@react-three/drei';
+import {
+  CameraControls,
+  Environment,
+  Float,
+  MeshReflectorMaterial,
+  RenderTexture,
+  Text,
+  useFont,
+
+} from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+// import { useAtom } from "jotai";
+import { useEffect, useRef } from "react";
+import { Color } from "three";
+import { degToRad, lerp } from "three/src/math/MathUtils";
 import { Camping } from "./Camping";
+// import { currentPageAtom } from "./UI";
 
 
 export const Experience = () => {
+  const controls = useRef();
+
+  const intro = async () => {
+    controls.current.dolly(-22);
+    controls.current.smoothTime = 1.6;
+    controls.current.dolly(22,true)
+  };
+
+
+  useEffect(() => {
+    intro();
+  }, []);
   return (
     <>
-      <OrbitControls />
+      <CameraControls ref={controls} />
       <Text font={"fonts/Poppins-Black.ttf"}
         position-x={-1.3}
         position-y={-0.5}
         position-z={1}
         lineHeight={0.8}
         textAlign="center"
-        // rotation-y={degToRad(30)}
+        rotation-y={degToRad(30)}
         anchorY={"bottom"}>
          WHY  I  LOVE{"\n"}CAMPING
-        <meshBasicMaterial color= "white"/>
+        <meshBasicMaterial color= "white" >
+
+          <RenderTexture attach={"map"}>
+            <color attach="background" args={["#fff"]} />
+            <Environment preset="sunset" />
+            <Float floatIntensity={4} rotationIntensity={5}>
+              {/* <Camping
+                scale={2.5}
+                rotation-y={-degToRad(25)}
+                rotation-x={degToRad(40)}
+                position-y={4}
+              /> */}
+            </Float>
+          </RenderTexture>
+        </meshBasicMaterial>
       </Text>
-      <Camping />
+      <group rotation-y={degToRad(-25)} position-x={4}>
+      <Camping scale={2}/>
+      </group>
+      <mesh position-y={-0.48} rotation-x={-Math.PI / 2}>
+        <planeGeometry args={[100, 100]} />
+        <MeshReflectorMaterial
+          blur={[100, 100]}
+          resolution={2048}
+          mixBlur={1}
+          mixStrength={10}
+          roughness={1}
+          depthScale={1}
+          opacity={0.5}
+          transparent
+          minDepthThreshold={0.4}
+          maxDepthThreshold={1.4}
+          color="#333"
+          metalness={0.5}
+        />
+      </mesh>
       <Environment preset="sunset" />
     </>
   );
